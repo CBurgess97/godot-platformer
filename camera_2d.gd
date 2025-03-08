1cb32a9e3e732e50dc9c671d80a80b7a3194fa38
Callum <callumburgess997@gmail.com>
2025-03-08 22:03:44 +0000

    Add Smoother Camera Movement

--- camera_2d.gd
+++ camera_2d.gd
@@ -1,8 +1,15 @@
 extends Camera2D
 
-@export var player: Node2D = null
+@export var target: Node2D = null
+@export var damping: float = 5.0  # How quickly the camera catches up (higher = faster)
 
-func _process(_delta: float) -> void:
-    if player != null:
-        position = player.position
-        pass
+# Internal variables
+var player: Node2D  # Reference to the player node
+
+func _ready():
+	# Check if target is assigned
+	if not target:
+		push_warning("Camera2D: No target node assigned!")
+
+func _physics_process(delta):
+	position = position.lerp(target.position, damping * delta)
