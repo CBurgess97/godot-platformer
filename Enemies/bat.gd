0e65cc68c5e3c03a771f6fc0065e3e555fb81178
Callum <callumburgess997@gmail.com>
2025-03-10 23:30:24 +0000

    Add Bad Hover Effect

--- Enemies/bat.gd
+++ Enemies/bat.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 @onready var animation : AnimatedSprite2D = $AnimatedSprite2D
+@onready var anim_player : AnimationPlayer = $AnimationPlayer
+
+@export var hover : float = 0
 
 var dead : bool = false
 var death_timer : Timer
@@ -8,6 +11,7 @@ var death_time : float = 0.4
 
 func _ready() -> void:
 	animation.play("fly")
+	anim_player.play("bat_hover")
 	death_timer = Timer.new()
 	add_child(death_timer)
 	death_timer.one_shot = true
@@ -16,6 +20,10 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if dead and death_timer.time_left == 0:
 		queue_free()
+
+func _physics_process(_delta: float) -> void:
+	position.y = hover
+	position = round(position)
 
 func death() -> void:
 	death_timer.start()
