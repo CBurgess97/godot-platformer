83548e5a98f2f2046bc8cbeda307b81af8d2adeb
Callum <callumburgess997@gmail.com>
2025-03-10 15:29:11 +0000

    Basic UI Implementation

--- camera_2d.gd
+++ camera_2d.gd
@@ -3,8 +3,11 @@ extends Camera2D
 @export var target: Node2D = null
 @export var track_vertical: bool = false
 @export var track_horizontal: bool = true
+@export var vertical_offset: float = -40.0
 @export var camera_horizontal_offset: float = 3.0
 @export var damping: float = 5.0  # How quickly the camera catches up (higher = faster)
+
+var real_position: Vector2 = Vector2()
 
 # Internal variables
 var player: Node2D  # Reference to the player node
@@ -16,7 +19,12 @@ func _ready():
 
 func _physics_process(delta):
 	if track_vertical:
-		position.y = lerp(position.y, target.position.y, damping * delta)
+		real_position.y = lerp(position.y, target.position.y, damping * delta)
 
 	if track_horizontal:
-		position.x = lerp(position.x, target.position.x, damping * delta) + camera_horizontal_offset
+		real_position.x = lerp(position.x, target.position.x, damping * delta) + camera_horizontal_offset
+	
+	
+	# snap position to whole values
+	position = real_position.round()
+	position.y += vertical_offset
