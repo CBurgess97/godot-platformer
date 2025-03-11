47de167345ad4893a73d63659f4ab0c3682efb23
Callum <callumburgess997@gmail.com>
2025-03-11 17:32:50 +0000

    Add Bounce to Spikes and Fix Hover Glitch

--- Levels/Level Objects/damage_on_contact_component.gd
+++ Levels/Level Objects/damage_on_contact_component.gd
@@ -4,4 +4,7 @@ class_name DamageOnContactComponent
 
 func on_enter_hitbox(node : Node) -> void:
 	if node.has_method("change_state"):
-		node.change_state("death")
+		if not node.dead:
+			node.change_state("death")
+	if not node.dead:
+		node.movement.bounce(200)
